6dcdad57f1701e467c7f7ab2af823aec27d7053b
Vitor Mussatto <mussatto@gmail.com>
2015-10-13 18:58:22 -0300

    fix bind

--- src/main/resources/public/js/logsearch.js
+++ src/main/resources/public/js/logsearch.js
@@ -1,17 +1,24 @@
 var logSearchApp = angular.module('logSearchApp', []);
 var searchUrl = "http://localhost:8080/services/search";
 
-logSearchApp.controller('SearchController', function ($scope) {
+logSearchApp.controller('SearchController', function ($scope, $http) {
 
   $scope.loglines=[];
 
   $scope.doSearch = function(){
     var queryString = $("#query").val();
 
-    $.post(searchUrl, {queryString: queryString}, function(loglines){
-            $scope.loglines=[];
-            $scope.loglines=loglines;
+    $.post(searchUrl,{"queryString":queryString},function(data){
+        $scope.$apply(function(){
+            $scope.loglines=data;
         });
+    });
+
+  };
+
+  function processLogline(data){
+    var logline={logLineNumber:"1", log:"my log"};
+    $scope.loglines.push(logline);
   };
 
 
